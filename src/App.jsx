5503fe93320e79ec1974bdba93a82f31d4834f15
Kishor Wadhekar <kwadhekar3@gmail.com>
2025-12-16 18:03:44 +0530

    My first React Blog Project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import TodoList from "./Component/Usejsx";
+
+
+function MyApp(){
+
+  return(
+    <div>
+<TodoList/>
+</div>
+  )
+
+}
+
+export default MyApp;
---
+++ src/Component/Usejsx.jsx
@@ -0,0 +1,23 @@
+
+function TodoList() {
+    function callme(){
+        alert("Function Called");   
+    }
+  return (
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img 
+        src="https://i.imgur.com/yXOvdOSs.jpg" 
+        alt="Hedy Lamarr" 
+        className="photo" 
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+      <button onClick={callme}>Click Me</button>
+    </>
+  );
+}
+export default TodoList;
